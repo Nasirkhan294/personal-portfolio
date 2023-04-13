64ad102d405c413a2d42ec717290592d9c960e90
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-04-13 18:12:40 +0500

    Modified in javascript file

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const toggle = document.querySelector('#menu-btn');
-const menu = document.querySelector('ul');
-
-toggle.onclick = () => {
-  if (menu.classList.contains('active')) {
-    toggle.classList.remove('fa-times');
-    menu.classList.remove('active');
-    toggle.style.color = '#6070ff';
-  } else {
-    toggle.classList.add('fa-times');
-    menu.classList.add('active');
-    toggle.style.color = '#fff';
-  }
-};
-
-window.onscroll = () => {
-  toggle.classList.remove('fa-times');
-  menu.classList.remove('active');
-  toggle.style.color = '#6070ff';
-};
-
 /* --------------------- Portfolio Item Details ---------------------- */
 const projects = [
   {
