9880019d675157e45e7d0a9a94382150b9aa72de
Nasir <nasirbhatti.nk@gmail.com>
2023-10-14 17:19:19 +0500

    Add custom mouse cursor

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ window.onscroll = () => {
   toggleMenu.classList.remove('active');
 };
 
+/* ============== Mouse cursor =========== */
+
+const cursor1 = document.querySelector('.cursor-1');
+const cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) => {
+  cursor1.style.top = `${e.pageY}px`;
+  cursor1.style.left = `${e.pageX}px`;
+  cursor2.style.top = `${e.pageY}px`;
+  cursor2.style.left = `${e.pageX}px`;
+};
+
+document.querySelectorAll('a').forEach((links) => {
+  links.onmouseenter = () => {
+    cursor1.classList.add('active');
+    cursor2.classList.add('active');
+  };
+
+  links.onmouseleave = () => {
+    cursor1.classList.remove('active');
+    cursor2.classList.remove('active');
+  };
+});
+
 /* --------------------- Portfolio Item Details ---------------------- */
 const projects = [
   {
